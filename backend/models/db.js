a15335210a0accc57410c7a4652eedf1d801b269
vaibhavkhatal <vaibhavkhatal@gmail.com>
2021-01-12 12:14:05 +0530

    Update db.js
    add!!

--- backend/models/db.js
+++ backend/models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true,   useCreateIndex: true, useUnifiedTopology: true },
      (err) => {
-    if (!err) { console.log('MongoDB connection succeeded!!'); }
+    if (!err) { console.log('MongoDB connection succeeded!!!!!!'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
 
